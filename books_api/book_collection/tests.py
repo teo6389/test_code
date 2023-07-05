726331df05d20323f5bbb6f5e4b28f0596913a53
teo6389 <teo6389@yahoo.gr>
2023-07-05 22:05:39 +0300

    added example unit tests

--- books_api/book_collection/tests.py
+++ books_api/book_collection/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from book_collection import views
 
-# Create your tests here.
+class BookCollectionTest(TestCase):
+    
+    def test_url_reponse(self):
+        response = self.client.get("/books/get_all_books/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_books_reponse(self):
+        self.assertIsNotNone(views.get_books({}))
+        # self.assertTrue(views.get_books({}) is True)
